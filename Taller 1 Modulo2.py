72fb14c58811f457de447fc9125f1d5d58dca608
JONNIER ANDRES TERAN MORALES <95320447+JonnierTeran@users.noreply.github.com>
2022-05-28 14:30:27 -0500

    Add files via upload
    taller 1 modulo 2 diplomado

---
+++ Taller 1 Modulo2.py
@@ -0,0 +1,26 @@
+# Taller N1 - Modulo 2
+# Autor: Jonnier Andres Teran Morales
+# Correo= Jonnier.teran@upb.edu.co
+# ID No. 502195
+# id: 1003064599
+# Cel: 3245644212
+    
+# Creamos las Variables a Utilizar con valores de prueba
+a=2
+b=3
+c=5
+d=7
+e=8
+f=1
+
+# Realizamos el procedimiento a 2 nuevas variables
+ecu1 = (a+(b/c))/(d+(e/f))
+Ecu2 = a-(b/(c-d))
+
+# Reasignamos valores entre las variables
+Ecu2 = ecu1
+ecu1 = Ecu2
+
+# Imprimimos por pantalla la Solucion Obtenida
+print(ecu1)
+print(Ecu2) 
